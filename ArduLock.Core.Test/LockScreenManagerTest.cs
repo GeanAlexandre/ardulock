48413f337ed9524f1b89893953a27e3a831d8b9a
Gean Alexandre da Silva <gean.silva@programmers.com.br>
2018-02-20 14:06:33 -0300

    added unit test

---
+++ ArduLock.Core.Test/LockScreenManagerTest.cs
@@ -0,0 +1,29 @@
+﻿using ArduLock.Core.Strategy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace ArduLock.Core.Test
+{
+    [TestClass]
+    public class LockScreenManagerTest
+    {
+        private Mock<ILockWorkStationStrategy> _lockWorkStationStrategy = new Mock<ILockWorkStationStrategy>();
+
+
+        [TestMethod]
+        public void Should_throw_Exception()
+        {
+            Assert.ThrowsException<NullReferenceException>(() => LockScreenManager.Use(null, m => m.LockNow()));
+            _lockWorkStationStrategy.Verify(x => x.Lock(), Times.Never());
+
+        }
+        [TestMethod]
+        public void Should_Call_LockNow()
+        {
+            LockScreenManager.Use(_lockWorkStationStrategy.Object, m => m.LockNow());
+            _lockWorkStationStrategy.Verify(x => x.Lock(), Times.Once());
+
+        }
+    }
+}
---
+++ ArduLock.Core.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArduLock.Core.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArduLock.Core.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2cc6c1c5-a4b9-478f-860d-2a0f59c9508c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
